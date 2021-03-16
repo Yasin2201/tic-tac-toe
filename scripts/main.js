c8b89aafe446220e9f1c6774bd3f5030e31767dc
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-03-16 19:54:06 +0000

    added playerTurn functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,60 @@
 const gameboard = (() => {
-    const board = 
-    ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+    const board = ["", "", "", "", "", "", "", "", ""];
 
-     console.log(board) //Remove
-
-    function _render() {
-        const getBoard = document.querySelectorAll("[data-index]");
+    const getBoard = document.getElementsByClassName('squareDiv');
+    
+    const render = () => {
 
         for (i = 0; i < board.length; i++) {
             getBoard[i].textContent = board[i]
         }
-
-        console.log(getBoard) //Remove 
     }
-    _render()
+    return {render, getBoard, board}
+})();
+gameboard.render()
+
+const displayController = (() => {
+
+    const getBoard = gameboard.getBoard
+    let getBoardArray = Array.prototype.slice.call(getBoard);
+    let turn = 0
+    
+    getBoardArray.forEach(element => {
+        element.addEventListener("click", function(e){
+          
+          if (turn == 0 && e.target.textContent == "") {
+            element.textContent = player1.marker;
+            gameboard.board.splice(element.id, 1, player1.marker)
+            console.log(e.target.textContent)
+            turn = 1
+          } else if(turn == 1 && e.target.textContent == "")  {
+            element.textContent = player2.marker;
+            gameboard.board.splice(element.id, 1, player2.marker)
+            console.log(e.target.textContent)
+            turn = 0
+          }
+            // console.log(gameboard.board[element.id]) 
+            console.log(gameboard.board)  
+            });      
+      });
 })();
 
-const gameFlow = (() => {
 
-})
+
+
+const gameResult = (() => {    
+    const checkWin = () => {
+
+          }
+    })();
+
+
+
+
 
 const Player = (playerName, marker) => {
     return {playerName, marker}
 };
+
+const player1 = Player('Player 1', "X")
+const player2 = Player('Player 2', "O")
