117d0bbafafcf6f9d151d42738df8fd14c68297f
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-03-16 21:15:30 +0000

    added winning logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,11 @@
 const gameboard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
-
-    const getBoard = document.getElementsByClassName('squareDiv');
-    
-    const render = () => {
-
-        for (i = 0; i < board.length; i++) {
-            getBoard[i].textContent = board[i]
-        }
-    }
-    return {render, getBoard, board}
+    return {board}
 })();
-gameboard.render()
 
 const displayController = (() => {
 
-    const getBoard = gameboard.getBoard
+    const getBoard = document.getElementsByClassName('squareDiv');
     let getBoardArray = Array.prototype.slice.call(getBoard);
     let turn = 0
     
@@ -33,8 +23,8 @@ const displayController = (() => {
             console.log(e.target.textContent)
             turn = 0
           }
-            // console.log(gameboard.board[element.id]) 
             console.log(gameboard.board)  
+            gameResult.checkWin()
             });      
       });
 })();
@@ -43,11 +33,42 @@ const displayController = (() => {
 
 
 const gameResult = (() => {    
+
     const checkWin = () => {
+    
+    let board = gameboard.board
 
-          }
-    })();
+        if (
+            board[0] == player1.marker && board[1] == player1.marker && board[2] == player1.marker ||
+            board[3] == player1.marker && board[4] == player1.marker && board[5] == player1.marker ||
+            board[6] == player1.marker && board[7] == player1.marker && board[8] == player1.marker ||
+            board[0] == player1.marker && board[3] == player1.marker && board[6] == player1.marker ||
+            board[1] == player1.marker && board[4] == player1.marker && board[7] == player1.marker ||
+            board[2] == player1.marker && board[5] == player1.marker && board[8] == player1.marker ||
+            board[0] == player1.marker && board[4] == player1.marker && board[8] == player1.marker ||
+            board[2] == player1.marker && board[4] == player1.marker && board[6] == player1.marker
+          ) {
+              console.log("X WINS")
+        } else if (
+            board[0] == player2.marker && board[1] == player2.marker && board[2] == player2.marker ||
+            board[3] == player2.marker && board[4] == player2.marker && board[5] == player2.marker ||
+            board[6] == player2.marker && board[7] == player2.marker && board[8] == player2.marker ||
+            board[0] == player2.marker && board[3] == player2.marker && board[6] == player2.marker ||
+            board[1] == player2.marker && board[4] == player2.marker && board[7] == player2.marker ||
+            board[2] == player2.marker && board[5] == player2.marker && board[8] == player2.marker ||
+            board[0] == player2.marker && board[4] == player2.marker && board[8] == player2.marker ||
+            board[2] == player2.marker && board[4] == player2.marker && board[6] == player2.marker
+        ) {
+            console.log("O WINS")
 
+        } else if (board == ["X", "O", "X", "O", "X", "O", "X", "O", "X"]){ //DOESNT WORK
+            console.log("DRAW")
+            console.log(board)
+        }
+    }
+return {checkWin}
+
+})();
 
 
 
@@ -58,3 +79,23 @@ const Player = (playerName, marker) => {
 
 const player1 = Player('Player 1', "X")
 const player2 = Player('Player 2', "O")
+
+
+// Array.prototype.multiIndexOf = function (el) { 
+//     var idxs = [];
+//     for (var i = this.length - 1; i >= 0; i--) {
+//         if (this[i] === el) {
+//             idxs.unshift(i);
+//         }
+//     }
+//     return idxs;
+// };
+
+// var abc = gameboard.board
+//     console.log(abc.multiIndexOf('X'));
+//     console.log(abc.multiIndexOf('O'));
+//     console.log(winningCombo[0])
+
+//     if (winningCombo[0] != abc.multiIndexOf('X')) {
+//         console.log("X Wins")
+//     }
