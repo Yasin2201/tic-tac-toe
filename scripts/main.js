cfc0a0daef8f8dafe781aec0b2a330cc90b37966
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-03-17 19:33:44 +0000

    added replay functionality

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ const gameboard = (() => {
 const displayController = (() => {
 
     const getBoard = document.getElementsByClassName('squareDiv');
+    const replayGame = document.querySelector('.replayBtn')
     let getBoardArray = Array.prototype.slice.call(getBoard);
     let turn = 0
     
@@ -14,30 +15,36 @@ const displayController = (() => {
           
           if (turn == 0 && e.target.textContent == "") {
             element.textContent = player1.marker;
+            element.style.backgroundColor = "#cce5ff";
             gameboard.board.splice(element.id, 1, player1.marker)
-            console.log(e.target.textContent)
             turn = 1
+            
           } else if(turn == 1 && e.target.textContent == "")  {
             element.textContent = player2.marker;
+            element.style.backgroundColor = "#b3d7ff";
             gameboard.board.splice(element.id, 1, player2.marker)
-            console.log(e.target.textContent)
             turn = 0
           }
-            console.log(gameboard.board)  
             gameResult.checkWin()
-            });      
+            });   
+      
       });
+  
+    replayGame.addEventListener('click', () => {
+      location.reload();
+    });
+
 })();
 
 
-
-
-const gameResult = (() => {    
-
-    const checkWin = () => {
+const gameResult = (() => {  
+    const displayResult = document.querySelector('.announceWinner')
+    const modalContainer = document.querySelector('.modal')
     
+    const checkWin = () => {
     let board = gameboard.board
-
+    let playerWon = false
+      
         if (
             board[0] == player1.marker && board[1] == player1.marker && board[2] == player1.marker ||
             board[3] == player1.marker && board[4] == player1.marker && board[5] == player1.marker ||
@@ -48,7 +55,9 @@ const gameResult = (() => {
             board[0] == player1.marker && board[4] == player1.marker && board[8] == player1.marker ||
             board[2] == player1.marker && board[4] == player1.marker && board[6] == player1.marker
           ) {
-              console.log("X WINS")
+              modalContainer.style.display = 'block'
+              displayResult.textContent = "X WINS"
+              playerWon = true;
         } else if (
             board[0] == player2.marker && board[1] == player2.marker && board[2] == player2.marker ||
             board[3] == player2.marker && board[4] == player2.marker && board[5] == player2.marker ||
@@ -59,23 +68,23 @@ const gameResult = (() => {
             board[0] == player2.marker && board[4] == player2.marker && board[8] == player2.marker ||
             board[2] == player2.marker && board[4] == player2.marker && board[6] == player2.marker
         ) {
-            console.log("O WINS")
-
-        } else if (board == ["X", "O", "X", "O", "X", "O", "X", "O", "X"]){ //DOESNT WORK
-            console.log("DRAW")
-            console.log(board)
+            modalContainer.style.display = 'block'
+            displayResult.textContent = "O WINS"
+            playerWon = true;
+          
+        } else if (board.includes("") == false && playerWon == false) {
+             modalContainer.style.display = 'block'
+             displayResult.textContent = "IT'S A DRAW"
         }
-    }
+      }
+
 return {checkWin}
 
 })();
 
-
-
-
-const Player = (playerName, marker) => {
-    return {playerName, marker}
+const Player = (marker) => {
+    return {marker}
 };
 
-const player1 = Player('Player 1', "X")
-const player2 = Player('Player 2', "O")
+const player1 = Player("X")
+const player2 = Player("O")
